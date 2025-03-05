67d6dbc7f76f625262b8c3d767250698e22a7c9d
mtencza <mtencza@cs.stonybrook.edu>
2025-03-05 09:37:40 -0500

    restructure app and server to inject services

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,17 @@
 import express from 'express';
+import { EventProcessor } from './services/eventProcessor';
+import { ReportGenerator } from './services/reportGenerator';
+import { createRoutes } from './routes/routes';
 
-export const app = express();
-app.use(express.json());
+export function createApp(
+  eventProcessor: EventProcessor,
+  reportGenerator: ReportGenerator
+) {
+  const app = express();
+  app.use(express.json());
+  app.use('/', createRoutes(eventProcessor));
+
+  // scheduling for generating reports and maybe cleanup
+
+  return app;
+}
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,13 @@
-import { app } from './app';
+import { createApp } from './app';
+import { EventProcessor } from './services/eventProcessor';
+import { defaultConfig } from './config/appConfig';
+import { ReportGenerator } from './services/reportGenerator';
+
+const eventProcessor = new EventProcessor();
+const reportGenerator = new ReportGenerator(eventProcessor, defaultConfig);
+
+console.log('Initializing App');
+const app = createApp(eventProcessor, reportGenerator);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {

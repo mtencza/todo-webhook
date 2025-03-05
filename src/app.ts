a00b594078956ab75213a8ff76eab4bee7b73be2
mtencza <mtencza@cs.stonybrook.edu>
2025-03-05 10:42:46 -0500

    write reports to txt file

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { ReportGenerator } from './services/reportGenerator';
 import { createRoutes } from './routes/routes';
 import * as schedule from 'node-schedule';
 import { defaultConfig, getScheduleFromInterval } from './config/appConfig';
+import fs from 'node:fs/promises';
 
 export function createApp(
   eventProcessor: EventProcessor,
@@ -13,15 +14,19 @@ export function createApp(
   app.use(express.json());
   app.use('/', createRoutes(eventProcessor));
 
-  // scheduling for generating reports and maybe cleanup
-
   // Generate reports at interval & Clean up memory
   schedule.scheduleJob(
     getScheduleFromInterval(defaultConfig.reportInterval),
     async () => {
       console.log('Generating reports');
       const reports = await reportGenerator.generateReports();
-      //write reports somewhere
+
+      const now = new Date();
+      // Write to local txt file
+      fs.writeFile(
+        './report' + now.toISOString() + '.txt',
+        JSON.stringify(reports, null, 2)
+      );
 
       // get rid of previously deleted events
       //eventProcessor.cleanupOldDeletedTasks(defaultConfig.eventRetention);

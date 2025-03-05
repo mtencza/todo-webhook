587c146fb363172ef3d4640f21851ff681090d0d
mtencza <mtencza@cs.stonybrook.edu>
2025-03-05 10:30:12 -0500

    add scheduled job for generating reports

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { EventProcessor } from './services/eventProcessor';
 import { ReportGenerator } from './services/reportGenerator';
 import { createRoutes } from './routes/routes';
+import * as schedule from 'node-schedule';
+import { defaultConfig, getScheduleFromInterval } from './config/appConfig';
 
 export function createApp(
   eventProcessor: EventProcessor,
@@ -12,6 +14,19 @@ export function createApp(
   app.use('/', createRoutes(eventProcessor));
 
   // scheduling for generating reports and maybe cleanup
+
+  // Generate reports at interval & Clean up memory
+  schedule.scheduleJob(
+    getScheduleFromInterval(defaultConfig.reportInterval),
+    async () => {
+      console.log('Generating reports');
+      const reports = await reportGenerator.generateReports();
+      //write reports somewhere
+
+      // get rid of previously deleted events
+      //eventProcessor.cleanupOldDeletedTasks(defaultConfig.eventRetention);
+    }
+  );
 
   return app;
 }

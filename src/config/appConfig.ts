586c1b9ddea0cff18b1397d0e9820e1391f910ba
mtencza <mtencza@cs.stonybrook.edu>
2025-03-05 10:29:17 -0500

    interval conversion helper function

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -7,3 +7,19 @@ export const defaultConfig: AppConfig = {
   reportInterval: 'minute',
   maxRetention: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
+
+export function getScheduleFromInterval(
+  reportInterval: 'minute' | 'hourly' | 'daily'
+) {
+  switch (reportInterval) {
+    case 'minute':
+      return '*/1 * * * *'; // Every minute
+    case 'hourly':
+      return '0 * * * *'; // every hour
+    case 'daily':
+      return '0 0 * * *'; // Midnight every day
+    default:
+      console.warn(`Invalid interval: ${reportInterval}. Default to hourly.`);
+      return '0 * * * *';
+  }
+}
